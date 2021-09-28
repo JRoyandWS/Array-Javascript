1c53dbebc060e50fcdc949ed29b9b55c3803fc28
JRoyandWS <89475941+JRoyandWS@users.noreply.github.com>
2021-09-28 11:19:04 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,6 @@
+let recipe=["Pertama panas kanair","Selanjutnya, sambil menunggu mendidih campurkan 1 bungkus agar-agar dan gula pasir kedalam air yang dipanaskan","Setelah itu, potong buah-buahan menjadi kecil (sesuai selera)","lalu, letakkan buah kedalam loyang/ cetakan (sesuai selera)","Kemudian, jika air yang dipanaskan sudah mendidih, segera tuangkan ke dalam cetakan/ loyang yang sudah diberi kombinasi buah.","Berikutnya, diamkan hingga dingin.","Langkah selanjutnya, simpan di lemari es agar rasa lebih lezat.","Terakhir, ambil agar-agar dari lemari pendingin dan santap."];
+
+let last_step=recipe[7];
+console.log("Langkah-langkah dalam membuat agar-agar campur buah");
+console.log("Langkah awal :" +recipe[0]);
+console.log("Langkah akhir :" +last_step);
---
+++ tugas2.js
@@ -0,0 +1,9 @@
+let my_bucket_list =["Travel Toba lake","Run a Marathon","Designing website"];
+my_bucket_list.push("watching movies");
+my_bucket_list.unshift("Go camping");
+my_bucket_list.pop();
+my_bucket_list.shift();
+
+
+
+console.log("My Bucket list :" +"",my_bucket_list);
